4e872cfccfe209c541c4ae0ac5e5e18a9c659b24
Vladimir P <vladimir_pogonin@hotmail.com>
2018-03-28 22:42:54 +0300

    HW: Coordinates for touch point
    Add function getTouchDataForPoint.
    
    Closes #8

--- ft5206.c
+++ ft5206.c
@@ -23,7 +23,14 @@
 #define FT5206_REGISTER_TOUCH3_YL 		(0x12)
 
 #define FT5206_REGISTER_TOUCH4_XH 		(0x15)
+#define FT5206_REGISTER_TOUCH4_XL 		(0x16)
+#define FT5206_REGISTER_TOUCH4_YH 		(0x17)
+#define FT5206_REGISTER_TOUCH4_YL 		(0x18)
 #define FT5206_REGISTER_TOUCH5_XH 		(0x1B)
+#define FT5206_REGISTER_TOUCH5_XL 		(0x1C)
+
+#define FT5206_REGISTER_TOUCH5_YH 		(0x1D)
+#define FT5206_REGISTER_TOUCH5_YL 		(0x1E)
 
 #define FT5206_REGISTER_ID_G_PERIODACTIVE (0x88)	/* The period of active status, value 3 to 14 (defualt 12) */
 #define FT5206_REGISTER_ID_G_CIPHER				(0xA3)  /* Chip vendor ID */
@@ -34,18 +41,46 @@
 #define FT5206_CHIP_VENDOR_ID	(0x55)
 #define FT5206_CTPM_VENDOR_ID	(0x86)
 
+#define FT5206_ID_G_THGROUP 		0x80//valid touching detect threshold [R/W]
+#define FT5206_ID_G_THPEAK 			0x81//valid touching peak detect threshold [R/W]
+#define FT5206_ID_G_THCAL 			0x82//the threshold when calculating the focus of touching[R/W]
+#define FT5206_ID_G_THDIFF	 		0x85//the threshold where the coordinatis differs from orig[R/W]
+#define FT5206_ID_G_CLTR	 			0x86//power control mode[R/W]
+
 #define I2C (I2C1)		/*i2c peritherial address*/
 #define GPIO_RST 		GPIOB
 
 /* Private typedefs ----------------------------------------------------------*/
 
 /* Private variables ---------------------------------------------------------*/
-static const uint8_t prvBaseRegisterForPoint[5] = {
-		FT5206_REGISTER_TOUCH1_XH,
-		FT5206_REGISTER_TOUCH2_XH,
-		FT5206_REGISTER_TOUCH3_XH,
-		FT5206_REGISTER_TOUCH4_XH,
-		FT5206_REGISTER_TOUCH5_XH
+typedef __packed struct
+{
+	uint8_t msb;
+	uint8_t lsb;
+}bytePair;
+const uint8_t bytePairElemCount = sizeof(bytePair)/sizeof(uint8_t);
+
+typedef __packed struct
+{
+	bytePair first;
+	bytePair second;
+}valuePair;
+const uint8_t valuePairElemCount = sizeof(valuePair)/sizeof(bytePair);
+
+static const bytePair xTouchPointsRegistersPairs[5] = {
+		{FT5206_REGISTER_TOUCH1_XH,FT5206_REGISTER_TOUCH1_XL},
+		{FT5206_REGISTER_TOUCH2_XH,FT5206_REGISTER_TOUCH2_XL},
+		{FT5206_REGISTER_TOUCH3_XH,FT5206_REGISTER_TOUCH3_XL},
+		{FT5206_REGISTER_TOUCH4_XH,FT5206_REGISTER_TOUCH4_XL},
+		{FT5206_REGISTER_TOUCH5_XH,FT5206_REGISTER_TOUCH5_XL}
+};
+
+static const bytePair yTouchPointsRegistersPairs[5] = {
+		{FT5206_REGISTER_TOUCH1_YH,FT5206_REGISTER_TOUCH1_YL},
+		{FT5206_REGISTER_TOUCH2_YH,FT5206_REGISTER_TOUCH2_YL},
+		{FT5206_REGISTER_TOUCH3_YH,FT5206_REGISTER_TOUCH3_YL},
+		{FT5206_REGISTER_TOUCH4_YH,FT5206_REGISTER_TOUCH4_YL},
+		{FT5206_REGISTER_TOUCH5_YH,FT5206_REGISTER_TOUCH5_YL}
 };
 
 void ft5x06Init()
@@ -57,10 +92,23 @@ void ft5x06Init()
 	//Pull down
 	GPIO_RST->ODR ^= 1<<5;
 	//Wait
+	//Todo: here need timer
+	//for(int i=0;i<10000;i++){__ASM("nop");}
 	timDelay(50);
 	
 	//Pull-up
 	GPIO_RST->ODR |= 1<<5;
+	//Todo: After reset 
+	//Need wait here.
+}
+
+uint8_t registerRead(uint8_t reg)
+{
+	uint8_t data = 0;
+	//Set data address
+	i2cWriteBytes((uint8_t)FT5206_ADDRESS,&reg,sizeof(data));
+	i2cReadBytes((uint8_t)FT5206_ADDRESS,&data,sizeof(data));
+	return data;
 }
 
 DeviceMode getDeviceMode()
@@ -83,13 +131,29 @@ uint32_t getNumOfTouchPoints()
 
 uint32_t getTouchDataForPoint(FT5206TouchCoordinate* pCoordinate, FT5206Point Point)
 {
-	uint8_t data[4] = {0x0};
-	data[0] = prvBaseRegisterForPoint[Point-1];
-	i2cWriteBytes((uint8_t)FT5206_ADDRESS,&data[0],1);
-	i2cReadBytes((uint8_t)FT5206_ADDRESS,data,sizeof(data));
+	valuePair pair;
+	pair.first = xTouchPointsRegistersPairs[Point-1];
+	pair.second = yTouchPointsRegistersPairs[Point-1];
 	
-	pCoordinate->x = ((data[0] & 0x0F) << 8) | data[1];
-	pCoordinate->y = ((data[2] & 0x0F) << 8) | data[3];
-	
-	return (uint32_t)((data[0] & 0xC0) >> 6); /*	Return event type of touch FT5206Event */
+	for(bytePair *bPair = &(pair.first);bPair<((&(pair.first))+sizeof(valuePair));bPair+=sizeof(bytePair))
+	{	
+		uint8_t* s = 	(uint8_t*)(bPair);
+		uint8_t* e = 	(uint8_t*)(bPair+sizeof(bytePair));
+
+		for(uint8_t* bytePtr = s; bytePtr<e; bytePtr++)
+		{	
+			i2cWriteBytes((uint8_t)FT5206_ADDRESS,bytePtr,1);
+			i2cReadBytes((uint8_t)FT5206_ADDRESS,bytePtr,1);	
+		}
+	}
+	pCoordinate->x = ((pair.first.msb & 0x0F) << 8) | pair.first.lsb;
+	pCoordinate->y = ((pair.second.msb & 0x0F) << 8) | pair.second.lsb;
+
+	return (uint32_t)(pair.first.msb &0xC0);
 }
+
+uint8_t getGestureId()
+{
+	return registerRead((uint8_t)FT5206_REGISTER_GEST_ID);
+}
+
--- ft5206.h
+++ ft5206.h
@@ -27,6 +27,23 @@ typedef enum
 	FT5206Event_Contact = 2,
 } FT5206Event;
 
+typedef enum
+{
+	FT5206Gest_MoveUp 		= 0x10,
+	FT5206Gest_MoveDown 	= 0x18,
+	FT5206Gest_MoveLeft 	= 0x14,
+	FT5206Gest_MoveRight 	= 0x1C,
+	FT5206Gest_ZoomIn 		= 0x48,
+	FT5206Gest_ZoomOut 		= 0x49,
+	FT5206Gest_NoGesture 	= 0
+} FT5206Gest;
+
+//typedef enum
+//{
+//	FT5206InterruptMode_Polling,
+//	FT5206InterruptMode_Trigger,
+//} FT5206InterruptMode;
+
 typedef struct
 {
 	uint16_t x;
@@ -43,5 +60,7 @@ void ft5x06Init(void);
 DeviceMode getDeviceMode(void);
 uint32_t getNumOfTouchPoints(void);
 uint32_t getTouchDataForPoint(FT5206TouchCoordinate* pCoordinate, FT5206Point Point);
+uint8_t getGestureId(void);
+uint8_t registerRead(uint8_t reg);
 
 #endif
